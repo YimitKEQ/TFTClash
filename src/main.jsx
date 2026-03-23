9fb9f5c1a8a963731d8b213c760139427da421be
Kees <lodiestream@gmail.com>
2026-03-23 04:42:28 +0100

    feat: add tailwind css global styles and material symbols fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import * as Sentry from '@sentry/react'
-import '@tabler/icons-webfont/dist/tabler-icons.min.css'
+import './index.css'
 import App from './App.jsx'
 
 Sentry.init({
