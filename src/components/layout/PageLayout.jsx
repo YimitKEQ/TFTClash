8f0300f14157023dc1d93b2a0ab58b56399def77
Kees <lodiestream@gmail.com>
2026-05-03 18:26:58 +0200

    fix(layout): sticky footer eliminates empty space below short pages

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -5,11 +5,11 @@ import TournamentStatusStrip from './TournamentStatusStrip'
 export default function PageLayout({ children, maxWidth }) {
   var mw = maxWidth || 'max-w-7xl'
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-background flex flex-col">
       <Navbar />
-      <div className="pt-20">
+      <div className="pt-20 flex-1 flex flex-col">
         <TournamentStatusStrip />
-        <main className="pb-24 md:pb-12 px-4 md:px-8">
+        <main className="pb-24 md:pb-12 px-4 md:px-8 flex-1">
           <div className={mw + ' mx-auto py-8'}>
             {children}
           </div>
